2b90b8fb614e8c942833f8f05d2b98c810151b9f
Moriarty <danguohang@polytopetech.com>
2020-03-07 14:07:50 +0800

    add imu and image seperately, but seems something not right

--- main.cc
+++ main.cc
@@ -10,9 +10,17 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <glog/logging.h>
+#include <gflags/gflags.h>
+#include <sensor_msgs/Imu.h>
 
 #include "Thirdparty/DLib/FileFunctions.h"
 
+DEFINE_string(data_path, "", "path to datas");
+DEFINE_string(filename, "/bag.bag", "bag file name, will be put into data_path");
+DEFINE_string(data_ext, ".png", "image format, with dot like .png");
+DEFINE_string(left_image_topic, "/cam0/image_raw", "image topic");
+DEFINE_string(right_image_topic, "/cam1/image_raw", "image topic");
+DEFINE_string(imu_topic, "/imu_raw", "imu topic");
 
 using namespace std;
 
@@ -32,45 +40,120 @@ unsigned long long get_time_from_string(std::string &name) {
   return t;
 };
 
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "BagFromImages");
-
-  if (argc != 5) {
-    LOG(FATAL) << "Usage: rosrun BagFromImages BagFromImages"
-                  " <path to image directory> <image extension .ext> "
-                  "<frequency> <path to output bag>";
-    return 0;
-  }
-
-  ros::start();
-
+void write_image_into_bag(
+    const std::string &path, rosbag::Bag &bag, const std::string &topic) {
   // Vector of paths to image
   vector<string> filenames =
-      DUtils::FileFunctions::Dir(argv[1], argv[2], true);
+      DUtils::FileFunctions::Dir(path.c_str(), FLAGS_data_ext.c_str(), true);
+  LOG(ERROR) << "Images: " << filenames.size() << " path " << path;
 
-  LOG(ERROR) << "Images: " << filenames.size() << endl;
-  // Output bag
-  rosbag::Bag bag_out(argv[4], rosbag::bagmode::Write);
-  double t;
+  unsigned long long ts;
+  double second_t;
+
   for (size_t i = 0; i < filenames.size(); i++) {
     if (!ros::ok())
       break;
 
     cv::Mat im = cv::imread(filenames[i], cv::IMREAD_GRAYSCALE);
-    cv_bridge::CvImage cvImage;
-    cvImage.image = im;
-    cvImage.encoding = sensor_msgs::image_encodings::TYPE_8UC1;
-    ///this part should be the same with image read from file, I set it to IMREAD_GRAYSCALE
+    if (!im.empty()) {
+      cv_bridge::CvImage cvImage;
+      cvImage.image = im;
+      cvImage.encoding = sensor_msgs::image_encodings::TYPE_8UC1;
+      ///this part should be the same with image read from file, I set it to IMREAD_GRAYSCALE
 
-    unsigned long long this_time = get_time_from_string(filenames[i]);
-    t = this_time /
-        1000000000.0; //in my system, time is ns, so make it /1000000000
-    cvImage.header.stamp = ros::Time(t);
-    bag_out.write("/camera/image_raw",ros::Time(t),cvImage.toImageMsg());
-    LOG(ERROR) << i << " / " << filenames.size() << endl;
+      ts = get_time_from_string(filenames[i]);
+      second_t = ts /
+                 1000000000.0; //in my system, time is ns, so make it /1000000000
+      cvImage.header.stamp = ros::Time(second_t);
+      bag.write(topic.c_str(),
+                ros::Time(second_t), cvImage.toImageMsg());
+//      LOG(ERROR) << i << " / " << filenames.size() << endl;
+    } else {
+      LOG(ERROR) << "image " << filenames[i] << " read fail";
+    }
+  }
+}
+
+void write_imu_into_bag(
+    const std::string &path, rosbag::Bag &bag, const std::string &topic){
+  std::ifstream imu_data(path);
+  std::stringstream ss;
+  unsigned long long ts;
+  double second_t;
+  if (imu_data.is_open()) {
+    const int imu_size = 6;
+    std::vector<double> imu(imu_size);
+    char comma;
+    std::string line;
+    sensor_msgs::ImuPtr imu_msg = boost::make_shared<sensor_msgs::Imu>();
+    while (!imu_data.eof()) {
+      std::getline(imu_data, line);
+      if (line.empty()) {
+        break;
+      }
+      ss.str(line);
+      ss >> ts;
+      ss >> comma;
+      CHECK(comma == ',');
+      for (int i = 0; i < imu_size; i++) {
+        ss >> imu[i];
+        ss >> comma;
+        CHECK(comma == ',');
+      }
+      second_t = ts /
+                 1000000000.0; //in my system, time is ns, so make it /1000000000
+      imu_msg->header.stamp = ros::Time(second_t);
+      imu_msg->angular_velocity.x = imu[0];
+      imu_msg->angular_velocity.y = imu[1];
+      imu_msg->angular_velocity.z = imu[2];
+      imu_msg->linear_acceleration.x = imu[3];
+      imu_msg->linear_acceleration.y = imu[4];
+      imu_msg->linear_acceleration.z = imu[5];
+
+      bag.write(topic.c_str(),
+          ros::Time(second_t), imu_msg);
+    }
+    imu_data.close();
+  } else {
+    LOG(ERROR) << "no imu data in " << path;
+  }
+}
+
+int main(int argc, char **argv) {
+  google::SetVersionString("1.0.0");
+  google::SetUsageMessage(std::string(argv[0]) + " [OPTION]");
+  google::ParseCommandLineFlags(&argc, &argv, false);
+  google::InitGoogleLogging(argv[0]); // option --[also]logtostderr
+  //        --stderrthreshold=0
+  ros::init(argc, argv, "BagFromImages");
+
+  CHECK(!FLAGS_data_path.empty())
+      << "Usage: rosrun BagFromImages BagFromImages"
+        " <path to image directory> <image extension .ext> "
+        "<frequency> <path to output bag>";
+
+  std::string image_path[2] = {FLAGS_data_path + "/cam0/data/", FLAGS_data_path + "/cam1/data/"};
+
+  ros::start();
+  // Output bag
+  std::string bagname = FLAGS_data_path + FLAGS_filename;
+  rosbag::Bag bag_out(bagname.c_str(), rosbag::bagmode::Write);
+
+  if (!FLAGS_left_image_topic.empty()) {
+    write_image_into_bag(image_path[0], bag_out, FLAGS_left_image_topic);
+  }
+
+  if (!FLAGS_right_image_topic.empty()) {
+    write_image_into_bag(image_path[1], bag_out, FLAGS_right_image_topic);
+  }
+
+  if (!FLAGS_imu_topic.empty()) {
+    std::string imu_file = FLAGS_data_path + "/imu0/data.csv";
+    write_imu_into_bag(imu_file, bag_out, FLAGS_imu_topic);
   }
 
   bag_out.close();
   ros::shutdown();
+  LOG(ERROR) << "finish write";
   return 0;
 }
